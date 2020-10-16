992002562f3d9c186f49b03937145489cbc078b0
ho11owd <51119622+ho11owd@users.noreply.github.com>
2020-10-16 16:58:53 +0800

    10-16

--- database/migrations/2020_10_08_062513_create_contracts_table.php
+++ database/migrations/2020_10_08_062513_create_contracts_table.php
@@ -15,7 +15,17 @@ class CreateContractsTable extends Migration
     {
         Schema::create('contracts', function (Blueprint $table) {
             $table->id();
+            $table->string('official_id')->unique();
+            $table->string('user_id')->constrained('userss', 'id');
+            $table->string('details');
+            $table->foreignId('contract_type_id')->constrained('contract_types', 'id');
             $table->timestamps();
+        });
+
+        Schema::create('contract_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps('2');
         });
     }
 
@@ -26,6 +36,7 @@ class CreateContractsTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('contract_types');
         Schema::dropIfExists('contracts');
     }
 }
